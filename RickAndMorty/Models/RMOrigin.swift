ea5f4cc963eaeb22b1b1accbaa05d1cb8129be50
blosinski <brendan.losinski@yahoo.com>
2023-04-22 01:43:59 -0500

    Add models

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Brendan Losinski on 4/22/23.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String 
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Brendan Losinski on 4/22/23.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String 
+}
